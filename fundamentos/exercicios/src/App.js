45302a79db27675cd194bcd42e03e7b158f47c9f
diegobaena89 <diegobf.cello@gmail.com>
2021-03-24 10:11:45 -0300

    add new react exercises

---
+++ fundamentos/exercicios/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Primeiro from './componentes/Primeiro'
+
+const App = () => {
+  return (
+    <div>
+      <Primeiro />
+    </div>
+  )
+}
+
+export default App
---
+++ fundamentos/exercicios/src/componentes/Primeiro.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+//Componente FUNCIONAL  - Baeado em função
+//função que renderiza uma parte ou componente da página
+
+export default () => <h1>Primeiro!</h1>;
