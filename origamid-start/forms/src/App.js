f55175527a60d4159048d60b42e97190a36b0a45
diegobaena89 <diegobf.cello@gmail.com>
2021-03-24 09:04:12 -0300

    add forms

--- origamid-start/forms/src/App.js
+++ origamid-start/forms/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Input from './Form/Input'
+import Radio from './Form/Radio';
 import Select from './Form/Select';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [produto, setProduto] = React.useState('');
+  const [cor, setCor] = React.useState('');
 
   return (
     <form>
+      <Radio options={['Azul', 'Vermelho']} value={cor}  setValue={setCor}/>
+      <br />
       <Select options={['smartphone', 'notebook']} value={produto} setValue={setProduto}/>
       <br />
       <Input id="nome" label="Nome" value={nome} setValue={setNome}/>
---
+++ origamid-start/forms/src/Form/Radio.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Radio = ({options, value, setValue}) => {
+  return (
+    <>
+    {options.map((option) => (
+    <label key={option}>
+        <input type="radio" value={option} checked={value === option}
+        onchange={({target}) => setValue
+        (target.value)}/>
+        {option}
+    </label>
+    ))}
+    </>
+  )
+}
+
+export default Radio
